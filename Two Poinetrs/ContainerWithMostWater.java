bafc9a12c50ca751037b050f0a3b57e2433b418c
Gokhul26 <gokhul2005@gmail.com>
2024-11-07 19:44:16 +0530

    Added Container with most water

---
+++ Two Poinetrs/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+class ContainerWithMostWater {
+    public static void main(String[] args) {
+
+            public int maxArea(int[] height) {
+                int left = 0;
+                int right = height.length - 1;
+                int maxArea = 0;
+                while(left < right){
+                    if(height[left] < height[right]){
+                        maxArea = Math.max(maxArea, (height[left] * (right - left)));
+                        left +=1;
+                    }else{
+                        maxArea = Math.max(maxArea, (height[right] * (right - left)));
+                        right -= 1;
+                    }
+                }
+                return maxArea;
+                
+            }
+        }
+
+}
