199b0f683be90bd62732985473b928f100439e20
Gokhul26 <gokhul2005@gmail.com>
2025-01-01 18:59:56 +0530

    booyah

---
+++ Merge-Sorted-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = 0;
+        for(int i = m; i < m+n; i++){
+            nums1[i] = nums2[index];
+            index++;
+        }
+        for(int i = 0; i < nums1.length; i++){
+            for(int j = i + 1; j < nums1.length; j++){
+                int temp = 0;
+                if(nums1[j] < nums1[i]){
+                    temp = nums1[i];
+                    nums1[i] = nums1[j];
+                    nums1[j] = temp;
+                }
+            }
+        }
+    }
+}
